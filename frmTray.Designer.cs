f37dc767c8850a92611e205d6c4921be3183965b
Craig <cledebur@gmail.com>
2017-02-19 23:33:47 -0800

    Restored frmTray

---
+++ frmTray.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace NUTty_UPS_Client
+{
+    partial class frmTray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBatteryPercentage = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblBatteryPercentage
+            // 
+            this.lblBatteryPercentage.AutoSize = true;
+            this.lblBatteryPercentage.Font = new System.Drawing.Font("Segoe UI Black", 25.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBatteryPercentage.Location = new System.Drawing.Point(25, 9);
+            this.lblBatteryPercentage.Name = "lblBatteryPercentage";
+            this.lblBatteryPercentage.Size = new System.Drawing.Size(216, 92);
+            this.lblBatteryPercentage.TabIndex = 0;
+            this.lblBatteryPercentage.Text = "100%";
+            this.lblBatteryPercentage.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 65);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "50 min";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // frmTray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 853);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblBatteryPercentage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmTray";
+            this.ShowIcon = false;
+            this.Text = "v";
+            this.Load += new System.EventHandler(this.frmTray_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBatteryPercentage;
+        private System.Windows.Forms.Label label1;
+    }
+}
